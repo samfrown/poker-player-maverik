a9c9795d49aa09865b61e6f233032f37d7dfba44
Yury.Alioshinov <yury.alioshinov@billing.ru>
2017-08-12 11:36:09 +0300

    fff

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,7 +2,6 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-import java.util.Map;
 
 public class Player {
 
@@ -15,43 +14,4 @@ public class Player {
 
     public static void showdown(JsonElement game) {
     }
-}
-
-int getCardRank(List<Card> cards) {
-
-    String url = "http://rainman.leanpoker.org/rank";
-
-    URL obj = null;
-    try {
-        obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        // optional default is GET
-        con.setRequestMethod("GET");
-
-        //add request header
-        con.setRequestProperty("User-Agent", USER_AGENT);
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        //print result
-        System.out.println(response.toString());
-    } catch (MalformedURLException e) {
-        e.printStackTrace();
-    }
-
-
-}
 }
