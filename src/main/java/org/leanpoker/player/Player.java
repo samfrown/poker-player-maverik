b85cdc3d06f1bcbc238a420ef01b96af3521b0fc
Kostya Samoylov <Konstantin.Samoylov@billing.ru>
2017-08-12 12:28:34 +0300

    add getMyCards

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,12 +1,15 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 
 public class Player {
 
 
     static final String VERSION = "Maverik";
+    static final String NAME = "Maverik";
 
     public static int betRequest(JsonElement request) {
         return 1000;
@@ -37,4 +40,33 @@ public class Player {
         }
 
     }
+
+    public static Cards getMyCards(JsonElement request) {
+        Cards my = new Cards();
+
+        try {
+            my.add(request.getAsJsonObject().get("community_cards").getAsJsonArray());
+        } catch (Exception e) {
+            System.err.println("community_cards not found");
+        }
+
+        JsonElement player = getMyPlayer(request);
+        if ( player != null ) {
+            my.add(player.getAsJsonObject().get("hole_cards").getAsJsonArray());
+        } else {
+            System.err.println("My player not found");
+        }
+        return my;
+    }
+
+    public static JsonElement getMyPlayer(JsonElement request) {
+        JsonArray players = request.getAsJsonObject().get("players").getAsJsonArray();
+        for (JsonElement player: players) {
+            if (player.getAsJsonObject().get("name").getAsString().equalsIgnoreCase(NAME)) {
+                return player;
+            }
+        }
+        return null;
+    }
+
 }
