3cb065b731c3c24ef1602f160ae17cb86ad4d252
Yury.Alioshinov <yury.alioshinov@billing.ru>
2017-08-12 10:49:57 +0300

    First fix

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,10 +6,10 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "Maverik";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        return 1000;
     }
 
     public static void showdown(JsonElement game) {
