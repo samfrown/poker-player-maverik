629413f4f3ef40f204c882c99296be8d3b51ed22
Yury.Alioshinov <yury.alioshinov@billing.ru>
2017-08-12 11:49:34 +0300

    stratagy2

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,9 +9,32 @@ public class Player {
     static final String VERSION = "Maverik";
 
     public static int betRequest(JsonElement request) {
-        return 1000;
+        return fff(request);
     }
 
     public static void showdown(JsonElement game) {
+    }
+
+
+    static int fff(JsonElement req) {
+        int round = req.getAsJsonObject().get("round").getAsInt();
+        int maxBet = req.getAsJsonObject().get("current_by_in").getAsInt();
+
+        int ourBet = 200;
+
+        if (round == 0) {
+
+        } else if (round < 3) {
+            ourBet = 500;
+        } else {
+            ourBet = 1000;
+        }
+
+        if (maxBet > ourBet) {
+            return maxBet;
+        } else {
+            return ourBet;
+        }
+
     }
 }
